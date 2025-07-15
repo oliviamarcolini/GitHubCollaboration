01a2576f3ff19f51f2c19fbcdb269a396c7285c6
Olivia Marcolini <oliviamonteiro2406@gmail.com>
2025-07-15 11:15:11 -0300

    Initial Commit

---
+++ GitHubCollaboration/GitHubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollaborationApp.swift
+//  GitHubCollaboration
+//
+//  Created by Olivia Monteiro on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
